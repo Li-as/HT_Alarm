6f46a09f6e0f8152c0292f7e75d082c4f730bae8
Lias <haseo.vr@gmail.com>
2020-07-10 12:10:45 +0300

    Init

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    private float _bottomLimit = 0.01f;
+    private bool _isThiefInside;
+
+
+    private void Update()
+    {
+        if (_isThiefInside)
+        {
+            _audioSource.volume = Mathf.Lerp(_audioSource.volume, 1, Time.deltaTime);
+
+        }
+        else if (_audioSource.isPlaying)
+        {
+            _audioSource.volume = Mathf.Lerp(_audioSource.volume, 0, Time.deltaTime);
+
+            if (_audioSource.volume <= _bottomLimit)
+            {
+                _audioSource.Stop();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Thief>(out Thief thief))
+        {
+            _isThiefInside = true;
+            _audioSource.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Thief>(out Thief thief))
+        {
+            _isThiefInside = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(Vector3.right * _speed * Time.deltaTime * - 1);
+        }
+    }
+}
